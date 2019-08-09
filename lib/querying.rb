b1d0b212cb4f3fa5da9c2d85347c4ccfbbdd4a37
Eric Clark <eclark0013@gmail.com>
2019-08-09 14:12:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ end
 def select_series_title_with_most_human_characters
   "SELECT * FROM series
   JOIN characters ON series.id = characters.series_id
-  WHERE characters.species = 'human'
+  WHERE characters.species = human
   GROUP BY series.id"
 end
 
